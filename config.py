89f3633519ba9278c629cc0a1e362804914d188b
Ronan-Francis <ronananthonyfrancis@gmail.com>
2025-01-19 17:33:10 +0000

    Refactor classification.py to remove debug print statement Update config.py with animal and non-animal training paths

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-directory_path = r"C:\Users\rf4thyrvm\Documents\CritterSnap\data\example\as_conservationistFrankfurt\IE_Forest_County_Wicklow_21_loc_01-20241031T145429Z-001\IE_Forest_County_Wicklow_21_loc_01"  # Adjust this with your correct path
-# directory_path = r"C:\Users\rf4thyrvm\Documents\CritterSnap\data\example\eccv_18_all_images_sm"  # Adjust this with your correct path
+# directory_path = r"C:\Users\rf4thyrvm\Documents\CritterSnap\data\example\as_conservationistFrankfurt\IE_Forest_County_Wicklow_21_loc_01-20241031T145429Z-001\IE_Forest_County_Wicklow_21_loc_01"  # Adjust this with your correct path
+directory_path = r"C:\Users\rf4thyrvm\Documents\CritterSnap\data\example\eccv_18_all_images_sm"  # Adjust this with your correct path
 output_directory = r"C:\Users\rf4thyrvm\Documents\CritterSnap\data\output"  # Adjust this with your correct path
 output_log_path = r"C:\Users\rf4thyrvm\Documents\CritterSnap\data\output\output_log.txt"  # Adjust this with your correct path
 change_threshold = 0.7 # Adjust this based on your images
@@ -15,4 +15,5 @@ base_url = "https://lilawildlife.blob.core.windows.net/lila-wildlife/wcs-unzippe
 start_file = 1    # Starting file number
 end_file = 700    # Ending file number
 
-
+animal_training_path = r"C:\Users\rf4thyrvm\Documents\CritterSnap\data\example\as_conservationistFrankfurt\IE_Forest_County_Wicklow_21_loc_01-20241031T145429Z-001\IE_Forest_County_Wicklow_21_loc_01"  # Adjust this with your correct path
+non_animal_training_path = r"C:\Users\rf4thyrvm\Documents\CritterSnap\data\example\eccv_18_all_images_sm"  # Adjust this with your correct path
